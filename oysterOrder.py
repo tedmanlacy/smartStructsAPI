5ee8c4ea37a491126f2641ccecf972b93b2fe33c
Ted Lacy <tedmanlacy@gmail.com>
2024-10-15 17:23:20 -0400

    feat: updated endpoints for OH

--- oysterOrder.py
+++ oysterOrder.py
@@ -1,15 +1,23 @@
-from fastapi import APIRouter, FastAPI
+from typing import Union
+from pydantic import BaseModel
+from fastapi import FastAPI
 
-# Create a new router
-router = APIRouter()
 
-# Define a new endpoint
-@router.get("/")
-async def new_endpoint():
-    return {"message": "Hello, this is a new oyster order!"}
-
-# Create the FastAPI app
 app = FastAPI()
 
-# Include the router in the app
-app.include_router(router)
+class Order(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/orders/{order_id}")
+def read_item(order_id: int, q: Union[str, None] = None):
+    return {"order_id": order_id, "q": q}
+
+@app.put("/orders/{order_id}")
+def update_item(order_id: int, order: Order):
+    return {"order_name": order.name, "order_id": order_id}
